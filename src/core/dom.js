88301af22c95ab23504e8b325fb27f1d7635685a
kinokus <aleks.kinokus@gmail.com>
2020-05-29 19:42:05 +0300

    dom $ ready

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,8 +1,42 @@
 class Dom {
+	constructor(selector) {
+		this.$el = typeof selector === 'string'
+			? document.querySelector(selector)
+			: selector
+	}
+
+	html(content) {
+		if (typeof content === 'string') {
+			this.$el.innerHTML = content
+			return this
+		}
+		return this.$el.outerHTML.trim()
+	}
+
+	clear() {
+		this.html('')
+		return this
+	}
+
+	append(node) {
+		if (node instanceof Dom) {
+			node = node.$el
+		}
+		if (Element.prototype.append) {
+			this.$el.append(node)
+		} else {
+			this.$el.appendChild(node)
+		}
+	}
+
+	addClass(className) {
+		this.$el.classList.add(className)
+		return this
+	}
 }
 
-export function $() {
-	return new Dom()
+export function $(selector) {
+	return new Dom(selector)
 }
 
 $.create = (tagname, classes = '') => {
@@ -10,5 +44,5 @@ $.create = (tagname, classes = '') => {
 	if (classes) {
 		el.classList.add(classes)
 	}
-	return el
+	return $(el)
 }
